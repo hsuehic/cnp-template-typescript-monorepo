e901983ee3d457678e5ac18d04e1018a2111779f
Xiaowei Xue <xiaowei.xue@shopee.com>
2023-08-21 14:32:23 +0800

    fix: exlint issue

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,9 +3,9 @@ module.exports = {
   extends: ['cnp', 'cnp/typescript', 'cnp/jest'],
   overrides: [
     {
-      files: '*.ts?(x)',
+      files: '*.{ts,mts,cts,tsx}',
       parserOptions: {
-        project: './tsconfig.json',
+        project: 'tsconfig.eslint.json',
       },
     },
   ],
